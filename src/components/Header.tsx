c919ea0f4d8f13b53c75038ee1c793e3101ebbb0
Rafael Rios <rvrios22@gmail.com>
2025-08-15 16:09:22 -0700

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@heroui/react'
+import { Link } from '@tanstack/react-router'
+
+export default function Header() {
+  return (
+    <header className="p-2 flex gap-2 bg-white text-black justify-between">
+      <nav className="flex flex-row">
+        <div className="px-2 font-bold">
+          <Link to="/">Home</Link>
+        </div>
+        <Button color='primary'>Test</Button>
+      </nav>
+    </header>
+  )
+}
