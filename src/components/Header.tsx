1ae374e89ee58ed3415a8e7facfcef707582d124
Rafael Rios <rvrios22@gmail.com>
2025-08-18 06:00:23 -0700

    add: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,79 @@
-import { Button } from '@heroui/react'
+import {
+  Button,
+  Dropdown,
+  Navbar,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  NavbarMenuToggle,
+} from '@heroui/react'
 import { Link } from '@tanstack/react-router'
+import InspectionButton from './InspectionButton'
+import { ChevronDown } from 'lucide-react'
+import pestLibrary from '../../public/pestLibrary'
+import { useState } from 'react'
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
   return (
-    <header className="p-2 flex gap-2 bg-white text-black justify-between">
-      <nav className="flex flex-row">
-        <div className="px-2 font-bold">
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <p className="font-bold text-inherit">Advance Pest Solutions</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent justify='end' className='hidden sm:flex gap-4'>
+        <Dropdown>
+          <NavbarItem>
+            <DropdownTrigger>
+              <Button
+                disableRipple
+                className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+                endContent={<ChevronDown />}
+                radius="sm"
+                variant="light"
+              >
+                Pest Library
+              </Button>
+            </DropdownTrigger>
+          </NavbarItem>
+          <DropdownMenu>
+            {pestLibrary.map((pest, idx) => (
+              <DropdownItem key={`${pest}-${idx}`}>{pest.name}</DropdownItem>
+            ))}
+          </DropdownMenu>
+        </Dropdown>
+        <NavbarItem>
+          <InspectionButton />
+        </NavbarItem>
+      </NavbarContent>
+
+      {/* mobile menu */}
+      <NavbarMenu className="justify-center items-center">
+        <NavbarMenuItem>
           <Link to="/">Home</Link>
-        </div>
-        <Button color='primary'>Test</Button>
-      </nav>
-    </header>
+        </NavbarMenuItem>
+        {pestLibrary.map((pest, index) => (
+          <NavbarMenuItem key={`${pest}-${index}`}>
+            <Link to={pest.link} className="w-full">
+              {pest.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+        <NavbarMenuItem>
+          <InspectionButton />
+        </NavbarMenuItem>
+      </NavbarMenu>
+    </Navbar>
   )
 }
---
+++ src/components/InspectionButton.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@heroui/react'
+
+function InspectionButton() {
+  return (
+    <Button
+      className="px-6 py-3 bg-gradient-to-r from-red-500 to-red-600 
+                text-white font-semibold rounded-md shadow-md 
+                transition-transform duration-200 ease-in-out 
+                hover:scale-105"
+    >
+      Get A Free Inspection
+    </Button>
+  )
+}
+
+export default InspectionButton
