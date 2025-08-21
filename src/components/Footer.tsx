cc5c6af81931b194b84b3cb07458de177a135f8b
Rafael Rios <rvrios22@gmail.com>
2025-08-21 07:41:41 -0700

    feat: links to socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ function Footer() {
       <div>
         <h3 className="text-3xl mb-2 mt-4 pt-4">Contact Us</h3>
         <p>Phone: 1-760-343-8622</p>
-        <p>Email: test@gmail.com</p>
-        <p>Address: 123 Street</p>
+        <p>Email: dionicio.madrid@advancepestsolutions.net</p>
+        <p>Address: Indio, CA</p>
       </div>
       <div>
         <h3 className="text-3xl mb-2 mt-4">Hours</h3>
@@ -16,9 +16,23 @@ function Footer() {
       <div>
         <h3 className="text-3xl mb-2 mt-4">Keep In Touch</h3>
         <div className="flex justify-around w-full">
-          <img src="./facebook.svg" alt="Facebook" width={40}/>
-          <img src="./instagram.svg" alt="Instagram" width={40}/>
-          <img src="./google.svg" alt="Google" width={40}/>
+          <a
+            href="https://www.facebook.com/profile.php?id=61568345716075#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="./facebook.svg" alt="Facebook" width={40} />
+          </a>
+          <a
+            href="https://www.instagram.com/advancepestsolutions/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="./instagram.svg" alt="Instagram" width={40} />
+          </a>
+          {/* <a href="http://" target="_blank" rel="noopener noreferrer">
+            <img src="./google.svg" alt="Google" width={40} />
+          </a> */}
         </div>
       </div>
     </footer>
