a79a14c2d57b0d7effd90f1ef07ad4521588b7e0
Rafael Rios <rvrios22@gmail.com>
2025-08-21 17:31:09 -0700

    feat:  form works and robots.txt and meta description

--- src/components/InspectionForm.tsx
+++ src/components/InspectionForm.tsx
@@ -1,81 +1,122 @@
 // src/components/InspectionForm.tsx
-import React, { useEffect, useRef } from 'react' // Add useEffect
+import React, { useEffect, useState, useRef } from 'react' // Add useEffect
 import { Form, Button, Input, Textarea, addToast } from '@heroui/react'
 import { Bug } from 'lucide-react'
 import useScrollStore from '../store/scrollStore' // Import your store
+import { useForm } from '@formspree/react'
 
-const InspectionForm = React.forwardRef<HTMLFormElement, {}>(
-  () => {
-    const formRef = useRef<HTMLFormElement>(null)
-    const setInspectionFormRef = useScrollStore(
-      (state) => state.setInspectionFormRef,
-    )
+const InspectionForm = React.forwardRef<HTMLFormElement, {}>(() => {
+  const [state, handleSubmit] = useForm('xanbrnro')
+  const [formData, setFormData] = useState({
+    name: '',
+    phone: '',
+    email: '',
+    desc: '',
+  })
+  const formRef = useRef<HTMLFormElement>(null)
+  const setInspectionFormRef = useScrollStore(
+    (state) => state.setInspectionFormRef,
+  )
 
-    useEffect(() => {
-      if (formRef.current) {
-        //@ts-ignore
-        setInspectionFormRef(formRef)
-      }
-    }, [setInspectionFormRef])
+  useEffect(() => {
+    if (formRef.current) {
+      //@ts-ignore
+      setInspectionFormRef(formRef)
+    }
+  }, [setInspectionFormRef])
 
-    interface InspectionFormEvent extends React.FormEvent<HTMLFormElement> {}
+  interface InspectionFormEvent extends React.FormEvent<HTMLFormElement> {}
 
-    const handleSubmit = (e: InspectionFormEvent): void => {
-      e.preventDefault()
+  const handleChange = (e: any) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  useEffect(() => {
+    if (state.succeeded) {
       addToast({
         title: 'We receieved your message!',
         description: "We'll be in contact to schedule as soon as possible.",
         icon: <Bug />,
       })
+      setFormData({
+        name: '',
+        phone: '',
+        email: '',
+        desc: '',
+      })
     }
 
-    return (
-      <Form
-        className="w-4/5 mx-auto lg:text-lg"
-        onSubmit={handleSubmit}
-        id="inspection-form"
-        ref={formRef}
-      >
-        <Input
-          isRequired
-          label="Name"
-          labelPlacement="inside"
-          isClearable
-          type="text"
-        />
-        <Input
-          isRequired
-          label="Phone Number"
-          labelPlacement="inside"
-          isClearable
-          type="tel"
-        />
-        <Input
-          isRequired
-          label="Email"
-          labelPlacement="inside"
-          isClearable
-          type="email"
-        />
-        <Textarea
-          isRequired
-          label="How Can We Help?"
-          labelPlacement="inside"
-          description="Let us know what pest issues you have so we can assist you"
-        />
-        <Button
-          type="submit"
-          className="mb-4 mx-auto px-6 py-3 bg-gradient-to-r from-red-500 to-red-600
+    if (state.errors) {
+      addToast({
+        title: 'Sorry, something went wrong',
+        description:
+          'Please try again or contact us via Facebook or Instagrame while we figure out our bugs.',
+        icon: <Bug />,
+      })
+    }
+  }, [state.succeeded, state.errors])
+  return (
+    <Form
+      className="w-4/5 mx-auto lg:text-lg"
+      onSubmit={handleSubmit}
+      action="https://formspree.io/f/xanbrnro"
+      method="post"
+      id="inspection-form"
+      ref={formRef}
+    >
+      <Input
+        isRequired
+        onChange={handleChange}
+        value={formData.name}
+        name="name"
+        label="Name"
+        labelPlacement="inside"
+        isClearable
+        type="text"
+      />
+      <Input
+        isRequired
+        onChange={handleChange}
+        value={formData.phone}
+        name="phone"
+        label="Phone Number"
+        labelPlacement="inside"
+        isClearable
+        type="tel"
+      />
+      <Input
+        isRequired
+        onChange={handleChange}
+        value={formData.email}
+        name="email"
+        label="Email"
+        labelPlacement="inside"
+        isClearable
+        type="email"
+      />
+      <Textarea
+        isRequired
+        onChange={handleChange}
+        value={formData.desc}
+        name="desc"
+        label="How Can We Help?"
+        labelPlacement="inside"
+        description="Let us know what pest issues you have so we can assist you"
+      />
+      <Button
+        isLoading={state.submitting}
+        disabled={state.submitting}
+        type="submit"
+        className="mb-4 mx-auto px-6 py-3 bg-gradient-to-r from-red-500 to-red-600
                 text-white font-semibold rounded-md shadow-md
                 transition-transform duration-200 ease-in-out
                 hover:scale-105 max-w-fit lg:text-lg"
-        >
-          Submit
-        </Button>
-      </Form>
-    )
-  },
-)
+      >
+        Submit
+      </Button>
+    </Form>
+  )
+})
 
 InspectionForm.displayName = 'InspectionForm'
 
